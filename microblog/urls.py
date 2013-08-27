f3fe5a2e3a68cd6742fc927062e82fa5c964402c
Dibayendu Dey <dibayendu.dey@fridaymedia.com.au>
2013-08-27 13:03:30 +0000

    added index and 500 templates

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -8,8 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^microblog/', include('microblog.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r"^$", TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', include(admin.site.urls)),
 )
