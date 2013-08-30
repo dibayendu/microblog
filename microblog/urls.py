5e0bdc59af1c5d0825e1e5c79ccec8e8fd11baa8
Dibayendu Dey <dibayendu.dey@fridaymedia.com.au>
2013-08-30 17:56:39 +0000

    urls and static_rrot

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 # from django.views.generic import TemplateView
@@ -16,4 +17,10 @@ urlpatterns = patterns('',
     url(r"^$", views.HomepageView.as_view(), name="home"),
     url(r"^blog/", include("blog.urls", namespace="blog")),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )
