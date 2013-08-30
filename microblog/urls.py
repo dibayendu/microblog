df3d0012d0104b3d99e67cdd3afb84ebf266caf9
Dibayendu Dey <dibayendu.dey@fridaymedia.com.au>
2013-08-30 16:35:21 +0000

    base template, blog view, templates

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
+
+from . import views
 
 admin.autodiscover()
 
@@ -9,6 +11,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'microblog.views.home', name='home'),
     # url(r'^microblog/', include('microblog.foo.urls')),
 
-    url(r"^$", TemplateView.as_view(template_name="index.html")),
+    # url(r"^$", TemplateView.as_view(template_name="index.html")),
+    
+    url(r"^$", views.HomepageView.as_view(), name="home"),
+    url(r"^blog/", include("blog.urls", namespace="blog")),
     url(r'^admin/', include(admin.site.urls)),
 )
